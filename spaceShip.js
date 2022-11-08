eb83956245eab419935659f3c91133249e957b7f
Karen Shiloh Jolly <karenshilohj@gmail.com>
2022-11-07 19:13:21 -0800

    updates...

--- spaceShip.js
+++ spaceShip.js
@@ -4,6 +4,20 @@
 //   `${name} moving to ${topSpeed}`
 
 
-
+class Spaceship {
+    constructor(name, topSpeed){
+        this.name = name;
+        this.topSpeed = topSpeed;
+    }
+    displaySpeed(){
+        console.log(`${this.name} moves at ${this.topSpeed}`);
+    }
+}
 // 2. Call the constructor with a couple ships, 
 // and call accelerate on both of them.
+
+const karenShip = new Spaceship('Karen', 'Superfast!');
+karenShip.displaySpeed();
+
+const cutieShip = new Spaceship('Cutie', 'Nice slow speed!');
+cutieShip.displaySpeed();
